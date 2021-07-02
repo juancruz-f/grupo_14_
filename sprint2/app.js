2a84ab9d4ceb49effafd5fcebbfd827618484ebc
Solyanett <abreu.solange@outlook.com>
2021-07-02 14:18:36 -0300

    creacion de register

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,6 +6,7 @@ app.use(express.static('public'))
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/productDetail',(req,res)=>res.sendFile(path.join(__dirname,'views','productDetail.html')))
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')))
 
 
 
