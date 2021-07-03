84bfeadaeccc204381985ab71d3313dabbd8c7c7
Araceli Belen Mamani Catacora <yeolsoo73@gmail.com>
2021-07-02 21:32:35 -0300

    agrego css a carrito

--- sprint2/app.js
+++ sprint2/app.js
@@ -7,6 +7,7 @@ app.use(express.static('public'))
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/productDetail',(req,res)=>res.sendFile(path.join(__dirname,'views','productDetail.html')))
+app.get('/productCart',(req,res)=>res.sendFile(path.join(__dirname,'views','productCart.html')))
 
 
 
