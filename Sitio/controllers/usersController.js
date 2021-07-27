344c8e3e15dba907466f1cc644194d5642d9a88b
Solyanett <abreu.solange@outlook.com>
2021-07-27 00:34:20 -0300

    vista y ruta de contacto y formulario de producto

---
+++ Sitio/controllers/usersController.js
@@ -0,0 +1,15 @@
+module.exports= {
+    login:(req,res)=>{
+        return res.render('login')
+    },
+
+
+    register:(req,res)=>{
+        return res.render('register')
+    },
+
+
+    contact:(req,res)=>{
+        return res.render('contact')
+    }
+}
--- Sitio/routes/users.js
+++ Sitio/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const{login,register,contact} = require('../controllers/usersController')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/login',login);
+router.get('/register',register);
+router.get('/contact',contact)
 
 module.exports = router;
