dfc4df193603f1ff06794637dd5acf137092ad9f
Solyanett <abreu.solange@outlook.com>
2021-07-27 22:27:45 -0300

    Json creado

--- Sitio/controllers/indexController.js
+++ Sitio/controllers/indexController.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const products= require('../data/products_db')
 module.exports = {
     index : (req, res)=>{
+        console.log(products)
         return res.render('index',{
             title : "Oh Shots"
     })
