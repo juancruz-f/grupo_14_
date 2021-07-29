782df9e3cce8e0e15f69199a1e3653ea56eba3ab
Solyanett <abreu.solange@outlook.com>
2021-07-28 23:10:08 -0300

    index filtrado

--- Sitio/controllers/indexController.js
+++ Sitio/controllers/indexController.js
@@ -7,6 +7,10 @@ module.exports = {
             title : "Oh Shots",
             products,
             sections,
+            destacados : products.filter(product => product.section === "destacados"),
+            interes: products.filter(product => product.section === "interes"),
+            packs : products.filter(product => product.section === "packs"),
     })
 }
 }
+
