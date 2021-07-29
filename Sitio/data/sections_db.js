3215d0ba8708e92a1d778da92f2a36c9a7566ac8
Sol Abreu <84038590+Solyanett@users.noreply.github.com>
2021-07-29 00:27:23 -0300

    Revert "Develop"

--- Sitio/data/sections_db.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, 'sections.json'),'utf-8'));
--- Sitio/routes/products.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {detail, cart, add} = require('../controllers/productsController')
-
-router.get('/detail/:id',detail);
-router.get('/cart',cart);
-router.get('/add',add)
-
-module.exports = router;
