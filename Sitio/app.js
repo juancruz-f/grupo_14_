bed4b956168c4deef3e8da6d26509882413f3348
Solyanett <abreu.solange@outlook.com>
2021-07-30 01:18:14 -0300

    terminado formulario de carga y edit

--- Sitio/app.js
+++ Sitio/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,7 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+app.use('/',indexRouter)
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
