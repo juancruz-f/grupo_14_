7d74c23aac2f667cc39a377dfb5a8c927b0ac181
Araceli Belen Mamani Catacora <yeolsoo73@gmail.com>
2021-08-26 07:12:30 -0300

    Multer en products

--- Sitio/routes/products.js
+++ Sitio/routes/products.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const {detail, cart, add, save, edit, update, remove,admin} = require('../controllers/productsController')
+const multer = require('multer')
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images'))
+    },
+    filename: (req, file, cb) => {
+        const newFilename = 'avatar' + Date.now() + path.extname(file.originalname)
+        cb(null, newFilename)
+    }
+
+})
+
+const upload = multer({storage : storage})
+
 
 router.get('/admin',admin);
 router.get('/detail/:id',detail);
 router.get('/cart',cart);
 router.get('/add',add);
-router.post('/add',save);
+router.post('/add',upload.single('avatar'),save);
 router.get('/edit/:id',edit);
 router.put('/edit/:id',update);
 router.post('/remove/:id',remove);
