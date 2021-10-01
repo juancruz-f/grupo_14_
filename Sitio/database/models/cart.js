66ed5fda665e87bb19a74d062ebcc36b0498718c
Araceli Belen Mamani Catacora <yeolsoo73@gmail.com>
2021-10-01 00:34:45 -0300

    modelo de cart

---
+++ Sitio/database/models/cart.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = "carts";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        createdAt: {
+            type: dataTypes.DATE,
+            allowNull: false,
+            unique: true
+        },
+        updatedAt: {
+            type: dataTypes.DATE,
+            allowNull: false,
+            unique: true
+        },
+        productId: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        userId: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        }
+
+    }
+
+    let config = {
+        tableName: "carts",
+        timestamps: false,
+        underscored: true
+
+    }
+
+    const carts = sequelize.define(alias, cols, config);
+
+    carts.associate = function (models) {
+        carts.hasMany(models.products, {
+            as: "products",
+            foreignKey: "productId",
+        })
+        carts.belongsTo(models.users, {
+            as: "users",
+            foreignKey: "userId",
+        })
+
+
+    }
+    return carts;
+}
