1398dee734df71f5a95c20079acf42fa2425effd
juancruz-f <juancruzfernandez108@gmail.com>
2021-10-28 22:00:28 -0300

    cambios menores

--- Sitio/database/config/config.js
+++ Sitio/database/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: 'root',
-    password: 'AndresCiro87Juan',
+    password: '',
     database: "ohshots",
     host: "127.0.0.1",
     dialect: "mysql",
